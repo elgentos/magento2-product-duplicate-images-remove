c40edbf38de18c9cc46c71a51c4caf5eabebafc1
peterjaap <peterjaap@elgentos.nl>
2024-07-05 09:10:56 +0200

    Moved source into src

--- src/registration.php
+++ src/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magegadgets_RemoveDuplicateImage',
+    'Elgentos_RemoveDuplicateImage',
     __DIR__
 );
